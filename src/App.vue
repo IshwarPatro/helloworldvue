b5ea6313dee1b7dc5cadea7d71d848448a6c88d3
Ishwar <ishwarp@smbxl.com>
2024-04-04 17:25:47 +0530

    C-3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <script setup>
-import GlobalStyle from './components/StylesExample/GlobalStyle.vue';
-import LocalStyle from './components/StylesExample/LocalStyle.vue';
-import CombinedStyle from './components/StylesExample/CombinedStyle.vue';
-import ModuleStyle from './components/StylesExample/ModuleStyle.vue';
+import MyEvents from './components/EventHandlers/MyEvents.vue';
 </script>
 
 <template>
-  <GlobalStyle />
-  <LocalStyle />
-  <CombinedStyle />
-  <ModuleStyle />
+  <MyEvents/>
 </template>
-
-<style scoped></style>
---
+++ src/components/EventHandlers/MyEvents.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue'
+let count =ref(0);
+
+const incrementCounter = () => count.value++
+const decrementCounter = () => count.value--
+
+</script>
+
+<template>
+    <h1>
+        Count: {{ count }}
+    </h1>
+    <button @:click="incrementCounter">Increment</button>
+    <button @:click="decrementCounter">Decrement</button>
+</template>
