b83cafe19d4597d5bdf7c3f31a071d6bd6023bdc
Ishwar <ishwarp@smbxl.com>
2024-04-05 17:19:20 +0530

    C-4

--- src/components/EventHandlers/MyEvents.vue
+++ src/components/EventHandlers/MyEvents.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!--<script setup>
 import {ref} from 'vue'
 let count =ref(0);
 
@@ -13,4 +13,35 @@ const decrementCounter = () => count.value--
     </h1>
     <button @:click="incrementCounter">Increment</button>
     <button @:click="decrementCounter">Decrement</button>
+</template>
+-->
+ 
+
+<!--Submit Handler-->
+<!--<script setup>
+const submitHandler = (e)=>{
+    e.preventDefault()
+    alert('Valid Credentials')
+}
+
+</script>
+
+<template >
+    <form @:submit="submitHandler">
+    <input type="text" placeholder="Please enter your name">
+    <input type="email" placeholder="Please enter your email">
+    <button type="submit">Submit</button>
+    </form>
+</template>
+-->
+
+<!--Show mwssage Handler-->
+<script setup>
+    const showMessage =(message)=>alert(message)
+</script>
+
+<template>
+    <button @click="showMessage('This is my message.')">
+        Show Message
+    </button>
 </template>
